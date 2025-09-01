ef82012a2c44a32dca1c3487d4d6c94b6254056e
Himanshu Maurya <himanshumaurya7781@gmail.com>
2025-09-01 10:40:30 +0530

    Fix logger method issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 connectDB()
     .then(() => {
         app.listen(PORT, () => {
-            logger.log(`Server running at: http://localhost:${PORT}`);
+            logger.info(`Server running at: http://localhost:${PORT}`);
         });
     })
     .catch((err) => {
