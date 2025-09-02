b2773bfa5d12babada1fb3dc239110decbd6f6e4
Himanshu Maurya <himanshumaurya7781@gmail.com>
2025-09-02 14:15:11 +0530

    Add verify JWT middleware

---
+++ backend/src/middlewares/user.middleware.js
@@ -0,0 +1,54 @@
+import jwt from 'jsonwebtoken';
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+
+import { asyncHandler } from '../utils/asyncHandler';
+import { ApiError } from '../utils/ApiError';
+import logger from "../utils/logger.js";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        // Extract token from "Authorization" header
+        const authHeader = req.headers["authorization"];
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            logger.warn("Missing or malformed Authorization header");
+            throw new ApiError(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED);
+        }
+
+        const token = authHeader.split(" ")[1];
+
+
+        // Verify token
+        let decodedToken;
+        try {
+            decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        } catch (err) {
+            logger.error("JWT verification failed", { error: err.message });
+            throw new ApiError(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED);
+        }
+
+        // Fetch user from DB
+        const user = await User.findById(decodedToken._id).select(
+            "-password -securityAnswer -refreshToken"
+        );
+
+        if (!user) {
+            logger.warn("User not found for decodedToken JWT", { userId: decodedToken._id });
+            throw new ApiError(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED);
+        }
+
+        // Attach user to request object
+        req.user = user;
+
+        logger.info("JWT verified successfully", { userId: user._id });
+        next();
+
+    } catch (error) {
+        // next(error);
+        throw new ApiError(
+            error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+            error.message || ReasonPhrases.INTERNAL_SERVER_ERROR,
+            error.errors || []
+        );
+    }
+});
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 
 import { validateRegister, validateLogin } from '../middlewares/validateUser.middleware.js';
-import { registerUser, loginUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/user.middleware.js";
 
 const router = Router();
 
 router.route('/register').post(validateRegister, registerUser);
 
 router.route('/login').post(validateLogin, loginUser);
+
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
