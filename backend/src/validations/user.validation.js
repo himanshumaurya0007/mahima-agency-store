0ae1d0e5053baa4cb337696cf021d837e9c545c0
Himanshu Maurya <himanshumaurya7781@gmail.com>
2025-09-02 13:02:40 +0530

    Add login validation schema

--- backend/src/validations/user.validation.js
+++ backend/src/validations/user.validation.js
@@ -97,3 +97,39 @@ export const userValidationSchema = Joi.object({
 
     refreshToken: Joi.string().optional(),
 });
+
+export const loginValidationSchema = Joi.object({
+    email: Joi.string()
+        .pattern(emailRegex)
+        .trim()
+        .lowercase()
+        .messages({
+            "string.pattern.base": errorMessages.EMAIL_INVALID,
+            "string.empty": errorMessages.REQUIRED(fields.email),
+        }),
+
+    username: Joi.string()
+        .min(3)
+        .max(30)
+        .pattern(usernameRegex)
+        .trim()
+        .lowercase()
+        .messages({
+            "string.pattern.base": errorMessages.USERNAME_INVALID,
+            "string.empty": errorMessages.REQUIRED(fields.username),
+            "string.min": errorMessages.MIN_LENGTH(fields.username, 3),
+            "string.max": errorMessages.MAX_LENGTH(fields.username, 30),
+        }),
+        
+        password: Joi.string()
+        .pattern(passwordRegex)
+        .min(8)
+        .max(100)
+        .required()
+        .messages({
+            "string.pattern.base": errorMessages.PASSWORD_INVALID,
+            "string.empty": errorMessages.REQUIRED(fields.password),
+            "string.min": errorMessages.MIN_LENGTH(fields.password, 8),
+            "string.max": errorMessages.MAX_LENGTH(fields.password, 100),
+        }),
+}).xor("username", "email");  // Ensure user provides either username OR email
