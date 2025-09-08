7b912ffaf8e93031a3202f26b2a7c2c797aa8e15
Himanshu Maurya <himanshumaurya7781@gmail.com>
2025-09-08 05:41:46 +0530

    Add ApiError, ApiResponse and asyncHandler utility files

---
+++ frontend/src/utils/ApiResponse.js
@@ -0,0 +1,15 @@
+// frontend/src/utils/ApiResponse.js
+class ApiResponse {
+    constructor(statusCode = 200, data = null, message = "Request successful") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode >= 200 && statusCode < 400;
+    }
+
+    static fromAxios(response) {
+        return new ApiResponse(response.status, response.data, response.statusText);
+    }
+}
+
+export default ApiResponse;
---
+++ frontend/src/utils/asyncHandler.js
@@ -0,0 +1,12 @@
+// frontend/src/utils/asyncHandler.js
+import ApiError from "./ApiError";
+
+export const asyncHandler = (fn) => {
+    return async (...args) => {
+        try {
+            return await fn(...args);
+        } catch (error) {
+            throw error instanceof ApiError ? error : ApiError.fromAxios(error);
+        }
+    };
+};
